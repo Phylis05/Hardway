7678ef0fe1412602a609ed5bba074985f60ab3eb
Phylis05 <chumba.phyl@gmail.com>
2019-09-27 13:10:05 +0300

    Move items to a new line and indent them

---
+++ ex10.rb
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in."  #\t spaces it beore the statement
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip \n\t* Grass    
+"""  # \n moves it to the next line
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
