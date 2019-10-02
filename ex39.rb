238030e3b267842b0d835f7702a73fbc853b9d2b
Phylis05 <chumba.phyl@gmail.com>
2019-10-02 16:38:31 +0300

    Iterate over hashes

---
+++ ex39.rb
@@ -0,0 +1,64 @@
+#create a mapping of state to abbreviation
+states = {
+	'Oregon' => 'OR',
+	'Florida' => 'FL',
+	'California' => 'CA',
+	'New York' => 'NY',
+	'Michigan' => 'MI'
+}
+#create a basic set of states and some cities in them
+cities = {
+	'CA' => 'San Francisco',
+	'MI' => 'Detroit',
+	'FL' => 'Jacksonvile'
+}
+#add some more cities
+cities ['NY'] = 'New York'
+cities ['OR'] = 'Portland'
+
+#puts out some cities
+puts '-' * 10
+puts "New York has: #{cities['NY']}"
+puts "Oregon has: #{cities['OR']}"
+
+#puts some states
+puts '-' * 10
+puts "Michichan's abbreviation is #{states['Michigan']}"
+puts "Florida's abbreviation is #{states['Floriida']}"
+
+#do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has:#{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
+
+#puts every state abreviation
+puts '-' * 10
+states.each do |state, abbrev|
+	puts "#{state} is abbreviated #{abbrev}"
+end
+
+#puts every city in state
+puts '-'* 10
+cities.each do|abbrev, city|
+	puts "#{abbrev} has the city #{city}"
+end
+
+#now do both at the same time
+puts '-' * 10
+states.each do |state, abbrev|
+	city = cities[abbrev]
+	puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+#by default ruby says "nil" when something isn'tin there
+state = states['Texas']
+
+if !state
+	puts "Sorry, no Texas."
+end
+
+#default values using //= with nil result
+city = cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"
